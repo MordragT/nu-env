310983bfda06831064c2a7dce9d509b1d9dfd317
Thomas Wehmöller <connect.mordrag@gmx.de>
2024-11-20 17:29:37 +0100

    🐛 Move nuenv into namespace

--- nupkgs/default.nix
+++ nupkgs/default.nix
@@ -1,10 +1,9 @@
 pkgs: let
   nuenv = import ../nuenv nuenv pkgs;
   callPackage = pkgs.lib.callPackageWith (pkgs // nuenv);
-in
-  nuenv
-  // {
-    all-to = callPackage ./all-to {};
-    hello = callPackage ./hello {};
-    nu-plugin-apt = callPackage ./nu-plugin-apt {};
-  }
+in {
+  inherit nuenv;
+  all-to = callPackage ./all-to {};
+  hello = callPackage ./hello {};
+  nu-plugin-apt = callPackage ./nu-plugin-apt {};
+}

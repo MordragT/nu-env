58de12a221aafb69a3e60d9a17b46694a7f0f999
Thomas Wehmöller <connect.mordrag@gmx.de>
2026-03-10 19:24:11 +0100

    update dependencies

--- nu-plugins/nu-plugin-apt/default.nix
+++ nu-plugins/nu-plugin-apt/default.nix
@@ -2,8 +2,9 @@
 rustPlatform.buildRustPackage {
   pname = "nu-plugin-apt";
   version = "0.1.0";
-  src = ../../.;
+  src = ../.;
 
-  cargoLock.lockFile = ../../Cargo.lock;
-  cargoBuildFlags = ["--package nu_plugin_apt"];
+  cargoLock.lockFile = ../Cargo.lock;
+  # Locked necessary: https://github.com/nushell/nushell/issues/17510
+  cargoBuildFlags = ["--package nu_plugin_apt" "--locked"];
 }
